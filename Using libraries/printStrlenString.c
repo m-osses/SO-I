f31404b69bfdeec90981bbc15c879fba56098317
Matias Osses <40499588+m0-2s-es@users.noreply.github.com>
2018-10-25 11:40:36 -0300

    Libreria <string.h>

---
+++ Using libraries/printStrlenString.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <stdio.h>
+
+int main()
+{
+char s[1000],
+unsigned i;
+printf("Enter a string: ");
+scanf("%s", s);
+i = strlen(s);
+printf("Length of string: %d", i);
+return 0;
+}
