647483ca0dd1a8c5b7d8a53dac7ee4041f195585
root <osses.matias@gmail.com>
2018-10-25 13:12:30 +0000

    se creo ejecutable y agrego return

--- Using libraries/printASCIIcharacter.c
+++ Using libraries/printASCIIcharacter.c
@@ -7,6 +7,6 @@ printf("Enter a character: ");
 scanf("%c", &c);
 // %d displays the integer value of a character
 // %c displays the actual character
-printf("ASCII value of %c = %d", c, c);
+printf("ASCII value of %c = %d \n", c, c);
 return 0;
 }
